f035c2c33a7395985d24cc21a24bbd9ef724082a
khiemta03 <khiemta.dev@gmail.com>
2024-06-29 00:41:28 +0700

    tao chuc nang dang tuyen dung

---
+++ ApplicationManagementSystem/BusinessLogic/PhieuThongTinDangTuyen_BUS.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationManagementSystem.BusinessLogic;
+using ApplicationManagementSystem.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationManagementSystem.BusinessLogic
+{
+    internal class PhieuThongTinDangTuyen_BUS 
+    {
+        public Guid MaPhieu { get; set; }
+        public string TenViTri { get; set; }
+        public int SoLuong { get; set; }
+        public string YeuCau { get; set; }
+        public string KhoangThoiGian { get; set; }
+        public string TinhTrang{ get; set; }
+        public string TinhTrangThanhToan { get; set; }
+        public string KieuThanhToan { get; set; }
+        public Guid MaPhieuDKTV { get; set; }
+        public string MaNhanVien { get; set; }
+
+        public static int DangKy(PhieuThongTinDangTuyen_BUS pdk)
+        {
+            return PhieuThongTinDangTuyen_DAO.Them(pdk);
+        }
+    }
+}
---
+++ ApplicationManagementSystem/DataAccess/PhieuThongTinDangTuyen_DAO.cs
@@ -0,0 +1,35 @@
+﻿using ApplicationManagementSystem.BusinessLogic;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationManagementSystem.DataAccess
+{
+    internal class PhieuThongTinDangTuyen_DAO
+    {
+        public static int Them(PhieuThongTinDangTuyen_BUS pdk)
+        {
+            string query = """
+				INSERT INTO PhieuTTDangTuyen(TenViTri, SoLuong, YeuCau, KhoangThoiGian, TinhTrang, TinhTrangTT, KieuTT, MaPhieuDKTV, MaNhanVien)
+				VALUES(@TenCongTy, @MaSoThue, @NguoiDaiDien, @DiaChi, @Email, @NgayTao, @TrangThai);
+				""";
+            var command = new SqlCommand(query, DbUtils.getInstance().Connection);
+
+            command.Parameters.Add("@TenViTri", SqlDbType.NVarChar).Value = pdk.TenViTri;
+            command.Parameters.Add("@SoLuong", SqlDbType.Int).Value = pdk.SoLuong;
+            command.Parameters.Add("@YeuCau", SqlDbType.Text).Value = pdk.YeuCau;
+            command.Parameters.Add("@KhoangThoiGian", SqlDbType.Text).Value = pdk.KhoangThoiGian;
+            command.Parameters.Add("@TinhTrang", SqlDbType.NVarChar).Value = pdk.TinhTrang;
+            command.Parameters.Add("@TinhTrangTT", SqlDbType.NVarChar).Value = pdk.TinhTrangThanhToan;
+            command.Parameters.Add("@KieuTT", SqlDbType.NVarChar).Value = pdk.KieuThanhToan;
+            command.Parameters.Add("@MaPhieuDKTV", SqlDbType.UniqueIdentifier).Value = pdk.MaPhieuDKTV;
+            command.Parameters.Add("@MaNhanVien", SqlDbType.NVarChar).Value = pdk.MaNhanVien;
+
+            return command.ExecuteNonQuery();
+        }
+    }
+}
